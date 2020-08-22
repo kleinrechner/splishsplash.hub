88b6cb0b0c74f37129f80f198d61f0b6e824b793
Christian Fluhrer <fluhrer@andavis.de>
2020-08-22 14:38:57 +0200

    Include NuGet-Package and -Feed

--- SplishSplash.Hub/Hubs/SplishSplashHub.cs
+++ SplishSplash.Hub/Hubs/SplishSplashHub.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Kleinrechner.SplishSplash.Backend.HubClientBackgroundService.Abstractions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Kleinrechner.SplishSplash.Hub.Hubs
 {
     [Authorize]
-    public class SplishSplashHub : Microsoft.AspNetCore.SignalR.Hub
+    public class SplishSplashHub : Microsoft.AspNetCore.SignalR.Hub<ISplishSplashHubClient>
     {
         public Task SendMessage(string user, string message)
         {
-            return Clients.All.SendAsync("ReceiveMessage", user, message);
+            //return Clients.All.SendAsync("ReceiveMessage", user, message);
+            return Task.CompletedTask;
         }
     }
 }
